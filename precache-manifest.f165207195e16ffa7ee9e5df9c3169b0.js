c238aacc5afa20fccaf1c99fc3456e64c0eb4f22
Matheus Graciano <krugler9@gmail.com>
2018-10-18 17:42:56 -0300

    Updates

---
+++ precache-manifest.f165207195e16ffa7ee9e5df9c3169b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9177da9392371211354222a28c6d47c",
+    "url": "/t02/static/media/foto.b9177da9.jpg"
+  },
+  {
+    "revision": "fcd52b3b5df14260870f",
+    "url": "/t02/static/css/main.3f5f966b.chunk.css"
+  },
+  {
+    "revision": "7dc0cf3292b5140557f8",
+    "url": "/t02/static/js/1.7dc0cf32.chunk.js"
+  },
+  {
+    "revision": "97b7c9de0faa9655122e",
+    "url": "/t02/static/js/runtime~main.97b7c9de.js"
+  },
+  {
+    "revision": "f779fd28e8bb03508fca7bda1a41d833",
+    "url": "/t02/static/media/background.f779fd28.jpg"
+  },
+  {
+    "revision": "fcd52b3b5df14260870f",
+    "url": "/t02/static/js/main.fcd52b3b.chunk.js"
+  },
+  {
+    "revision": "a642bf368af3a810a5af48f0960d44a8",
+    "url": "/t02/static/media/Currículo K.a642bf36.pdf"
+  },
+  {
+    "revision": "99f65fd10f19541fa8eae3714d894aa8",
+    "url": "/t02/static/media/cuda.99f65fd1.jpg"
+  },
+  {
+    "revision": "24b54adf6c4eb2d80df45f5cd86b449b",
+    "url": "/t02/static/media/git1.24b54adf.png"
+  },
+  {
+    "revision": "04f24a663eb013680b7b61c80bcf0df9",
+    "url": "/t02/static/media/back2.04f24a66.jpg"
+  },
+  {
+    "revision": "9b0ed3c776c38aacf0dd7784a6e987b7",
+    "url": "/t02/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.adc6f50d3ba4dc6b3baa766f76462b20.js"
+  "/t02/precache-manifest.f165207195e16ffa7ee9e5df9c3169b0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/t02/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
